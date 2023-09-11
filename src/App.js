c50eb78703ee9fcb375ce36d1e7c156ae47b0459
Rishad Achukatla <rishad.achukatla@acsicorp.com>
2023-09-11 15:41:26 +0530

    some1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Register from './Components/Register';
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
+      <BrowserRouter basename="https://rishad0701.github.io/manifest_application/">
       <Routes>
       <Route exact path="/" element = {<Home />}/>
       <Route exact path="/login" element = {<Login />}/>
